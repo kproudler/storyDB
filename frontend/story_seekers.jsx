bbd5da57bffb72c6e19d9d7e3c2a170b1fb20e46
Kevin Proudler <kevinproudler@gmail.com>
2020-08-25 12:11:27 -0400

    fe setup

---
+++ frontend/story_seekers.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const root = document.getElementById("root");
+    ReactDOM.render(<h1>Welcome to StorySeekers!</h1>, root);
+});
