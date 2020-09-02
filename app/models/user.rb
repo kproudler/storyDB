3233ae129a51d01449b78f4708c62e8fce93e8b4
Kevin Proudler <kevinproudler@gmail.com>
2020-09-02 16:57:19 -0400

    password min length validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
+    attr_reader :password
 
     validates :username, persence: true
     validates :password_digest, presence: { message: 'Password can\'t be blank' }
+    validates :password, length: { minimum: 6, allow_nil: true }
 
     def password=(password)
+        @password = password
         self.password_digest = BCrypt::Password.create(password)
     end
 
