0bc671b2d9c319376c21df7ab1cb5c5e07049297
Kevin Proudler <kevinproudler@gmail.com>
2020-09-02 11:53:30 -0400

    pw verification

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
         self.password_digest = BCrypt::Password.create(password)
     end
 
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
 end
